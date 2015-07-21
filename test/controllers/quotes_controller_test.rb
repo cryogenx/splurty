fd59c57016b0c1265c4deb2987a4585aa566334d
George Welsted <george@gwelsted.com>
2015-07-21 01:30:57 +0000

    Added FactoryGirl

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
 	test "quote show page" do
-    	quote = Quote.create(:author => 'Ken Mazaika', :saying => 'You only yolo once.')
-    	get :show, :id => quote.id
+    quote = FactoryGirl.create(:quote)
+    get :show, :id => quote.id
     	assert_response :success
 	end
 
